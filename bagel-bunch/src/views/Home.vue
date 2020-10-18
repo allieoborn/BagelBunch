c86b69994883ccf46958a16eb63f0228619de001
Eli Hermann <elihermann@Elis-MacBook-Pro.local>
2020-10-17 19:34:14 -0600

    Hey-o we got the site hosted, and talking to our backend

--- bagel-bunch/src/views/Home.vue
+++ bagel-bunch/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <BreadCrumb :current="currentPhase"/>
-    <button @click="cycle">Click Me</button>
+    <BreadCrumb :current="currentPhase" />
+    <button @click="firebase_test">CLICK ME FOR A MESSAGE FROM FIREBASE</button>
   </div>
 </template>
 
@@ -10,17 +10,33 @@ import BreadCrumb from "../components/BreadCrumb";
 
 export default {
   components: {
-    BreadCrumb: BreadCrumb
+    BreadCrumb: BreadCrumb,
   },
-  data () {
+  data() {
     return {
       currentPhase: "Bagels",
-    }
+    };
   },
   methods: {
-    cycle: function(){
+    cycle: function () {
       this.currentPhase = "Eli";
-    }
-  }
-}
+    },
+    firebase_test: function () {
+      fetch(
+        "https://us-central1-bagelbunch-b5e21.cloudfunctions.net/helloWorld"
+      )
+        .then((response) => response.json())
+        .then((value) => {
+          console.log(value.message);
+          var btn_div = document.createElement("div");
+          btn_div.style = "text-align: center;";
+          var btn = document.createElement("p");
+          btn.style = "font-size: 30px";
+          btn.innerHTML = value.message;
+          btn_div.appendChild(btn);
+          document.body.appendChild(btn_div);
+        });
+    },
+  },
+};
 </script>
