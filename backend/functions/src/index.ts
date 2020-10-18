7dbac5c6e586d5707b908aabecf6f4f9f870af14
Wesley Dashner <wdashner11@gmail.com>
2020-10-17 19:35:13 -0600

    fixed test cloud function

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -4,6 +4,7 @@ import * as functions from 'firebase-functions';
 // // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+ response.set('Access-Control-Allow-Origin', '*');
+ response.status(200).json({message: 'hello from firebase'});
 });
 
