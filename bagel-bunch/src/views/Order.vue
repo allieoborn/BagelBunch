5bf5d93eb16670a07da4da96b8a795103f8f29cd
Eli Hermann <elihermann@Elis-MacBook-Pro.local>
2020-11-20 18:04:36 -0700

    Add Item button adds a dish to the dishes list

--- bagel-bunch/src/views/Order.vue
+++ bagel-bunch/src/views/Order.vue
@@ -5,6 +5,18 @@
       <v-btn color="primary">Done</v-btn>
     </div>
     <hr />
+
+    <!-- Card for each dish in the order so far -->
+    <v-card v-for="d of this.order.dishes" :key="d.dish[0]">
+      <h4 class="pa-6">{{ d.dish[0] }}</h4>
+    </v-card>
+    <div
+      class="d-flex flex-row pa-6 align-center justify-center"
+      @click="addDish"
+    >
+      <h3 class="m-0 pr-6">Add Item</h3>
+      <v-icon x-large>mdi-plus-circle</v-icon>
+    </div>
   </v-container>
 </template>
 
@@ -19,9 +31,17 @@ export default {
         accountID: "", // string
         milliseconds: 0, // number
         cost: 0, // number
-        dishes: [{ dish: ["something"] }],
+        dishes: [
+          { dish: ["Original Bagel", "salsa"] },
+          { dish: ["Cheesy Bagel", "Something on it"] },
+        ],
       },
     };
+  },
+  methods: {
+    addDish() {
+      this.order.dishes.push({ dish: ["Cheesy Bagel"] });
+    },
   },
   created() {
     console.log(this.menu);
