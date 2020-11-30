464657a541ab6e2ad29e9a05df3e1f613da1d455
Eli Hermann <elihermann@Elis-MacBook-Pro.local>
2020-11-30 11:57:46 -0700

    You can now select a bagel and it updates a data field

--- bagel-bunch/src/components/EditItemOverlay/SelectBagelOptions/SelectBagelOptions.vue
+++ bagel-bunch/src/components/EditItemOverlay/SelectBagelOptions/SelectBagelOptions.vue
@@ -1,9 +1,19 @@
 <template>
   <div style="height: 100%" class="d-flex flex-row">
     <div class="scroller bagels">
-      <h3 class="my-0 py-2" v-for="bagel in menu.bagels" :key="bagel.name">
-        {{ bagel.name }}
-      </h3>
+      <v-item-group>
+        <div v-for="(bagel, i) in menu.bagels" :key="i">
+          <v-item v-slot="{ active, toggle }">
+            <v-card
+              class="ma-1"
+              :color="active ? 'primary' : 'secondary'"
+              @click="updateBagel(bagel, toggle)"
+            >
+              <h3 class="ma-0">{{ bagel.name }}</h3>
+            </v-card>
+          </v-item>
+        </div>
+      </v-item-group>
     </div>
 
     <div class="scroller bagels">
@@ -23,6 +33,19 @@
 import { mapGetters } from "vuex";
 export default {
   name: "SelectBagelOptions",
+  data() {
+    return {
+      bagel: null,
+      toppings: null,
+    };
+  },
+  methods: {
+    updateBagel(bagel, toggle) {
+      toggle();
+      this.bagel = bagel;
+      console.log(this.bagel);
+    },
+  },
   computed: {
     ...mapGetters({
       menu: "parsedMenu",
